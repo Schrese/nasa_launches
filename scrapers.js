393874a996e40a88e10688aae2aeffd74092f41b
Schrese <53787633+Schrese@users.noreply.github.com>
2020-05-31 13:09:40 -0400

    installed axios the queries aren't working yet, title only selecting from a meta tag description not selecting at all

--- scrapers.js
+++ scrapers.js
@@ -1,9 +1,60 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 
-let cheerio = require('cheerio');
-let $ = cheerio.load('https://www.nasa.gov/launchschedule/');
+axios.get('https://www.nasa.gov/launchschedule/')
+    .then(res => {
+        // console.log(res.data, 'what we want');
 
-var launchList = [];
+        const info = []; 
+        async function getting() {
 
-$('.list.items .item').each(function (index, element) {
 
+            const $ = cheerio.load(res.data);
+    
+            await $('title').each((index, element) => {
+                const title = $(element).text();
+                // console.log('hit here');
+                return info[index] = {...info, title}
+           
+            })
+
+            await $('.description').each((index, element) => {
+                const description = $(element).text();
+                // const description = 'world';
+                // console.log('hit here');
+                return info[index] = {...info, description}
+    
+            })
+
+            // return info = {
+            //     title,
+            //     description, 
+            //     date
+            // }
+        }
+
+        getting()
+        console.log(info);
+
+
+
+})
+
+
+
+
+
+
+
+// const $ = cheerio.load('https://www.wikipedia.org/');
+
+
+// $('.central-featured').each((i, e) => {
+
+//     const content = $(e).find('.central-featured-lang');
+
+//     launchList[i] = {content}
+
+// })
+// console.log(launchList);
